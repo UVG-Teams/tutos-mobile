252fed892e56e0a1162ca95eca47dd066f54d9c1
Marco <marcofuentes1607@gmail.com>
2020-08-16 13:07:13 -0600

    IP MARCO

--- tutos/src/settings.js
+++ tutos/src/settings.js
@@ -1,5 +1,6 @@
 // export const API_BASE_URL = 'http://localhost:8000/api'
-export const API_BASE_URL = 'http://192.168.1.5:3000/api'   // IP - W
+// export const API_BASE_URL = 'http://192.168.1.5:3000/api'   // IP - W
+export const API_BASE_URL = 'http://192.168.1.8:3000/api'   //IP - MARCO
 
 // Django token expiration = 1800 seconds
 export const tokenReviewTime = 300 * 1000 // Seconds * 1000
