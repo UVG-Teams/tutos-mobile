c046a56d319dd9e6328bf40565aa66b42c652f7c
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-08-16 16:44:15 -0600

    tutor dashboard style

--- tutos/src/app/home/index.js
+++ tutos/src/app/home/index.js
@@ -18,14 +18,18 @@ import {
     Title,
     Card,
     CardItem,
+    List,
+    Col,
+    Row,
 } from 'native-base'
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
 import { theme } from './../../layout/themes'
+import { connect } from 'react-redux'
 
 
-const Home = ({ navigation }) => (
+const Home = ({ navigation, tutorias}) => (
     <ImageBackground
         style={ theme.background }
     >
@@ -49,46 +53,70 @@ const Home = ({ navigation }) => (
             </Header>
             <Content style={ theme.content }>
                 <View>
-                    <Text style={{ fontSize: 35 }}>BIENVENIDO</Text>
-                    <Text style={{ fontSize: 20 }}>My Name</Text>
-
                     <Card>
-                        <CardItem header bordered button onPress={() => alert("Que onda, esto es header")}>
+                        <CardItem header bordered onPress={() => alert("Que onda, esto es header")}>
                             <Text style={{ fontSize: 18, fontWeight: "bold" }}>Programadas</Text>
                         </CardItem>
-                        <CardItem bordered button onPress={() => alert("Hola, esto es el body")}>
-                            <Body>
-                                <Text>
-                                    Tutoría Física 3 José Block
-                                </Text>
-                            </Body>
-                        </CardItem>
+                        {
+                            tutorias.map(tutoria => tutoria.status === "Programada" && (
+                                <CardItem bordered button onPress={() => alert("{tutoria.materia} {tutoria.tutorado}")}>
+                                    <Body>
+                                        <Row>
+                                            <Col>
+                                                <Text>{tutoria.materia} {tutoria.tutorado}</Text>
+                                            </Col>
+                                            <Col>
+                                                <Text style={{textAlign: 'right'}}>{tutoria.fechaHora}</Text>
+                                            </Col>
+                                        </Row>
+                                    </Body>
+                                </CardItem>
+                            ))
+                        }
                     </Card>
 
                     <Card>
-                        <CardItem header bordered button onPress={() => alert("Que onda, esto es header")}>
+                        <CardItem header bordered onPress={() => alert("Que onda, esto es header")}>
                             <Text style={{ fontSize: 18, fontWeight: "bold" }}>En curso</Text>
                         </CardItem>
-                        <CardItem bordered button onPress={() => alert("Hola, esto es el body")}>
-                            <Body>
-                                <Text>
-                                    Tutoría IPC Andy
-                                </Text>
-                            </Body>
-                        </CardItem>
+                        {
+                            tutorias.map(tutoria => tutoria.status === "En curso" && (
+                                <CardItem bordered button onPress={() => alert("Hola, esto es el body")}>
+                                    <Body>
+                                        <Row>
+                                            <Col>
+                                                <Text>{tutoria.materia} {tutoria.tutorado}</Text>
+                                            </Col>
+                                            <Col>
+                                                <Text style={{textAlign: 'right'}}>{tutoria.fechaHora}</Text>
+                                            </Col>
+                                        </Row>
+                                    </Body>
+                                </CardItem>
+                            ))
+                        }
                     </Card>
 
                     <Card>
-                        <CardItem header bordered button onPress={() => alert("Que onda, esto es header")}>
+                        <CardItem header bordered onPress={() => alert("Que onda, esto es header")}>
                             <Text style={{ fontSize: 18, fontWeight: "bold" }}>Terminadas</Text>
                         </CardItem>
-                        <CardItem bordered button onPress={() => alert("Hola, esto es el body")}>
-                            <Body>
-                                <Text>
-                                    Tutoría Estadística Willi
-                                </Text>
-                            </Body>
-                        </CardItem>
+                        {
+                            tutorias.map(tutoria => tutoria.status === "Terminada" && (
+                                <CardItem bordered button onPress={() => alert("Hola, esto es el body")}>
+                                    <Body>
+                                        <Row>
+                                            <Col>
+                                                <Text>{tutoria.materia} {tutoria.tutorado}</Text>
+                                            </Col>
+                                            <Col>
+                                                <Text style={{textAlign: 'right'}}>{tutoria.fechaHora}</Text>
+                                            </Col>
+                                        </Row>
+                                    </Body>
+                                </CardItem>
+                            ))
+                        }
                     </Card>
                 </View>
             </Content>
@@ -96,4 +124,14 @@ const Home = ({ navigation }) => (
     </ImageBackground>
 )
 
-export default Home
+export default connect(
+    state => ({
+        tutorias: [{tutorado: "Willi", materia: "Estadistica", status: "Programada", fechaHora: "25/08 15:00:00"},
+            {tutorado: "Andy", materia: "IPC", status: "Programada", fechaHora: "26/08 17:30:00"},
+            {tutorado: "Block", materia: "Fisica 3", status: "Programada", fechaHora: "27/08 15:00:00"},
+            {tutorado: "Willi", materia: "Estadistica", status: "En curso", fechaHora: "20/08 15:00:00"},
+            {tutorado: "Willi", materia: "Estadistica", status: "Terminada", fechaHora: "18/08 15:00:00"},
+            {tutorado: "Luca", materia: "Ecuaciones Diferenciales", status: "Terminada", fechaHora: "15/08 15:00:00"},
+            {tutorado: "Marco", materia: "Android", status: "Terminada", fechaHora: "11/08 15:00:00"}]
+    })
+)(Home);
