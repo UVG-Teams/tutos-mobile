f2267a29a88c0e60136b1aa1d2ac57d1638a755d
AndyCas99 <acastillo@devpackgroup.com>
2020-09-07 22:37:44 -0600

    Sagas update

--- tutos/src/tools/actions/signUp.js
+++ tutos/src/tools/actions/signUp.js
@@ -1,13 +1,24 @@
 import * as types from '../types/signUp';
 
-export const startSignUp = (username, password, firstName, lastName, email) => ({
+export const startSignUp = (username, password, firstName, lastName, email, birthdate, gender, isTutor,phone, price) => ({
   type: types.SIGN_UP_STARTED,
   payload: {
-    username,
-    password,
-    firstName,
-    lastName,
-    email,
+    user:{
+      username,
+      password,
+      firstName,
+      lastName,
+      email,
+      userDetail: {
+        gender,
+        phone,
+        birthdate,
+        is_tutor:isTutor,
+      },
+    },
+    tutor: {
+      individualPrice: price,
+    }
   },
 });
 
--- tutos/src/tools/sagas/signUp.js
+++ tutos/src/tools/sagas/signUp.js
@@ -14,27 +14,72 @@ import * as selectors from '../reducers';
 import * as types from '../types/signUp';
 import * as actions from '../actions/signUp';
 import * as authActions from '../actions/auth';
+import * as http from '../utils/http'
 
 
 function* signUp (action) {
   try{
+    const {user, userDetail, tutor} = action.payload
     const response = yield call(
       fetch,
       `${API_BASE_URL}/users/create_user/`,
       {
         method: 'POST',
-        body: JSON.stringify(action.payload),
+        body: JSON.stringify(user),
         headers:{
           'Content-type': 'application/json'
         },
       },
     );
+    const result = yield response.json();
+    const userId = result.id;
+    console.log(userId)
+    const detail = {
+        ...userDetail,
+        user_id:userId,
+    }
+    console.log(detail)
+    const tutorInfo={
+        ...tutor,
+        user_id:userId,
+    }
+    const detailResponse = true
+    // yield call(
+    //     fetch,
+    //     `${API_BASE_URL}/userdetails/`,
+    //     {
+    //         method: 'POST',
+    //         body: JSON.stringify(detail),
+    //         headers:{
+    //             'Content-Type': 'application/json',
+    //         },
+    //     },
+    // );
 
-    if(response.status == 200){
+    const tutorResponse = true
+    // yield call(
+    //     fetch,
+    //     `${API_BASE_URL}/tutores/`,
+    //     {
+    //         method: 'POST',
+    //         body: JSON.stringify(tutorInfo),
+    //         headers:{
+    //             'Content-Type': 'application/json',
+    //         },
+    //     },
+    // );
+
+    if(http.isSuccessful(response.status)&&http.isSuccessful(detailResponse.status)&&http.isSuccessful(tutorResponse.status)){
       const username = action.payload.username;
       const password = action.payload.password;
       yield put(actions.completeSignUp());
       yield put(authActions.startLogin(username, password));
+    } else if(http.isSuccessful(response.status)&& !http.isSuccessful(detailResponse.status)&&http.isSuccessful(tutorResponse.status)){
+      const { non_field_errors } = yield detailResponse.json();
+      yield put(actions.failSignUp(non_field_errors[0]));
+    } else if(http.isSuccessful(response.status)&& http.isSuccessful(detailResponse.status)&& !http.isSuccessful(tutorResponse.status)){
+      const { non_field_errors } = yield tutorResponse.json();
+      yield put(actions.failSignUp(non_field_errors[0]));
     } else {
       const { non_field_errors } = yield response.json();
       yield put(actions.failSignUp(non_field_errors[0]));
