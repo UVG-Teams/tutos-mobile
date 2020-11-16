9d9213c592d033d764d20fb7512d010500b88922
FR98 <ros18676@uvg.edu.gt>
2020-11-15 18:43:13 -0600

    Fix

--- tutos/src/tools/actions/location.js
+++ tutos/src/tools/actions/location.js
@@ -1,14 +1,14 @@
 import * as types from '../types/location'
 
 export const startFetchLocations = () => ({
-  type: types.START_FETCH_LOCATIONS,
-  payload: {},
+    type: types.START_FETCH_LOCATIONS,
+    payload: {},
 })
-export const finishFetchLocations = (data) => ({
-  type: types.FINISH_FETCH_LOCATIONS,
-  payload: data
+export const finishFetchLocations = data => ({
+    type: types.FINISH_FETCH_LOCATIONS,
+    payload: data
 })
-export const failFetchLocations = (error) => ({
-  type: types.FAIL_FETCH_LOCATIONS,
-  payload: error
+export const failFetchLocations = error => ({
+    type: types.FAIL_FETCH_LOCATIONS,
+    payload: error
 })
